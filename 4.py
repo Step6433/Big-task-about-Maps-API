12f50db592a1d8ad7f5b65ca4ceefadce87bb453
Step6433 <stepanaulchenko@gmail.com>
2025-03-24 20:46:36 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,64 @@
+from io import BytesIO
+import requests
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+spn = 0.005
+center_x = -0.124625
+center_y = 51.500706
+t = 'light'
+
+
+def get_map(spn, center_x, center_y, theme):
+    map_api_server = "https://static-maps.yandex.ru/v1?"
+    api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+    map_params = {
+        "ll": f'{center_x},{center_y}',
+        "spn": ",".join([str(spn), str(spn)]),
+        "l": "map",
+        "size": "600,400",
+        "theme": theme,
+        "apikey": api_key
+    }
+    response = requests.get(map_api_server, params=map_params)
+    if response.status_code == 200:
+        return BytesIO(response.content)
+    return
+
+
+running = True
+while running:
+    map = get_map(spn, center_x, center_y, t)
+    if map:
+        image = pygame.image.load(map)
+        screen.blit(image, (0, 0))
+        pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                if spn + 0.005 <= 2:
+                    spn += 0.005
+            elif event.key == pygame.K_PAGEDOWN:
+                if spn - 0.005 >= 0.005:
+                    spn -= 0.005
+            elif event.key == pygame.K_UP:
+                if center_y + 0.002 <= 60:
+                    center_y += 0.002
+            elif event.key == pygame.K_DOWN:
+                if center_y - 0.002 >= -60:
+                    center_y -= 0.002
+            elif event.key == pygame.K_LEFT:
+                if center_x - 0.002 >= -180:
+                    center_x -= 0.002
+            elif event.key == pygame.K_RIGHT:
+                if center_x + 0.002 <= 180:
+                    center_x += 0.002
+            elif event.key == pygame.K_t:
+                if t == 'light':
+                    t = 'dark'
+                else:
+                    t = 'light'
+pygame.quit()
